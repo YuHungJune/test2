bfcc7dfa9b60a687fe0a7f6684273d6d7eac80ac
YuHungJune <batort08@gmail.com>
2022-01-08 11:20:13 +0900

    test

--- src/test2.java
+++ src/test2.java
@@ -1,5 +1,11 @@
 public class test2 {
     public static void main(String[] args){
         System.out.println("Hello");
+        System.out.println("Hello1s");
+        System.out.println("Hello2");
+
+        while(true){
+
+        }
     }
 }
